dbb0f99408fe23a1b2b2548b352f4458028b59da
Grigorii Isupov <guisupov@gmail.com>
2024-11-25 23:38:35 +0300

    Homework 9 guisupov fix

--- homework/grigory_isupov/Homework_9/task_1.py
+++ homework/grigory_isupov/Homework_9/task_1.py
@@ -4,8 +4,8 @@ date = "Jan 15, 2023 - 12:05:33"
 
 python_date = datetime.datetime.strptime(date, '%b %d, %Y - %H:%M:%S')
 
-print(f'Месяц: {python_date.strftime('%B')}')
-print(f'{python_date.strftime('%d.%m.%Y, %H:%M')}')
+print(f"Месяц: {python_date.strftime('%B')}")
+print(f"{python_date.strftime('%d.%m.%Y, %H:%M')}")
 
 print(python_date)
 
