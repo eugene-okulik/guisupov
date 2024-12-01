bdf021f5abe0c08d094da2dfee71012b21ca3ae9
Grigorii Isupov <guisupov@gmail.com>
2024-12-01 23:06:01 +0300

    Homework 11 guisupov fix print

--- homework/grigory_isupov/Homework_11/task_1.py
+++ homework/grigory_isupov/Homework_11/task_1.py
@@ -9,23 +9,9 @@ class Books:
         self.isbn = isbn  # ISBN
         self.is_reserved = is_reserved
 
-
-school_book = Books("Война и мир", "Толстой", 100, "13: 978-2-266-11156-0", True)
-school_book_2 = Books("Мертвые души", "Гоголь", 50, "13: 978-2-266-11156-1")
-school_book_3 = Books("Мцирии", "Лермонтов", 130, "13: 978-2-266-11156-2")
-school_book_4 = Books("Собачье сердце", "Булгаков", 100, "13: 978-2-266-11156-3")
-school_book_5 = Books("Каштанка", "Чехов", 95, "13: 978-2-266-11156-5")
-
-print(f"Название: {school_book.title}, Автор: {school_book.author}, страниц: {school_book.page_count}, "
-      f"материал: {school_book.page_material}{', зарезервирована' if school_book.is_reserved else ''}")
-print(f"Название: {school_book_2.title}, Автор: {school_book_2.author}, страниц: {school_book_2.page_count}, "
-      f"материал: {school_book_2.page_material}{', зарезервирована' if school_book_2.is_reserved else ''}")
-print(f"Название: {school_book_3.title}, Автор: {school_book_3.author}, страниц: {school_book_3.page_count}, "
-      f"материал: {school_book_3.page_material}{', зарезервирована' if school_book_3.is_reserved else ''}")
-print(f"Название: {school_book_4.title}, Автор: {school_book_4.author}, страниц: {school_book_4.page_count}, "
-      f"материал: {school_book_4.page_material}{', зарезервирована' if school_book_4.is_reserved else ''}")
-print(f"Название: {school_book_5.title}, Автор: {school_book_5.author}, страниц: {school_book_5.page_count}, "
-      f"материал: {school_book_5.page_material}{', зарезервирована' if school_book_5.is_reserved else ''}")
+    def get_description(self):
+        return (f"Название: {self.title}, Автор: {self.author}, страниц: {self.page_count}, "
+                f"материал: {self.page_material}{', зарезервирована' if self.is_reserved else ''}")
 
 
 class SchoolBook(Books):
@@ -35,53 +21,30 @@ class SchoolBook(Books):
         self.class_room = class_room
         self.has_exercises = has_exercises
 
+    def get_description(self):
+        base_description = super().get_description()
+        return (base_description +
+                f", предмет: {self.subject}, класс: {self.class_room}")
 
-# Так вроде более читабельно
-school_book_6 = SchoolBook(
-    title="Алгебра",
-    author="Иванов",
-    page_count=200,
-    isbn="13: 978-2-266-11156-6",
-    subject="Математика",
-    class_room=9
-)
 
-school_book_7 = SchoolBook(
-    title="История государства",
-    author="Карамзин",
-    page_count=300,
-    isbn="13: 978-2-266-11156-7",
-    subject="История",
-    class_room=10
-)
+school_book_1 = Books("Война и мир", "Толстой", 100, "13: 978-2-266-1115-0", True)
+school_book_2 = Books("Мертвые души", "Гоголь", 50, "13: 978-2-266-11156-1")
+school_book_3 = Books("Мцирии", "Лермонтов", 130, "13: 978-2-266-11156-2")
+school_book_4 = Books("Собачье сердце", "Булгаков", 100, "13: 978-2-266-11156-3")
+school_book_5 = Books("Каштанка", "Чехов", 95, "13: 978-2-266-11156-5")
 
-school_book_8 = SchoolBook(
-    title="География",
-    author="Пржевальский",
-    page_count=430,
-    isbn="13: 978-2-266-11156-8",
-    subject="Записки о путешествиях",
-    class_room=9
-)
-print(f"Название: {school_book_6.title}, Автор: {school_book_6.author}, страниц: {school_book_6.page_count}, "
-      f"материал: {school_book_6.page_material}, "
-      f"предмет: {school_book_6.subject}, класс: {school_book_6.class_room}"
-      f"{', зарезервирована' if school_book_6.is_reserved else ''}")
+school_book_6 = SchoolBook(title="Алгебра", author="Иванов", page_count=200,
+                           isbn="13: 978-2-266-11156-6", subject="Математика", class_room=9)
 
-print(f"Название: {school_book_7.title}, Автор: {school_book_7.author}, страниц: {school_book_7.page_count}, "
-      f"материал: {school_book_7.page_material}, "
-      f"предмет: {school_book_7.subject}, класс: {school_book_7.class_room}"
-      f"{', зарезервирована' if school_book_7.is_reserved else ''}")
+school_book_7 = SchoolBook(title="История государства", author="Карамзин", page_count=300,
+                           isbn="13: 978-2-266-11156-7", subject="История", class_room=10)
 
-print(f"Название: {school_book_8.title}, Автор: {school_book_8.author}, страниц: {school_book_8.page_count}, "
-      f"материал: {school_book_8.page_material}, "
-      f"предмет: {school_book_8.subject}, класс: {school_book_8.class_room}"
-      f"{', зарезервирована' if school_book_8.is_reserved else ''}")
+school_book_8 = SchoolBook(title="География", author="Пржевальский", page_count=430,
+                           isbn="13: 978-2-266-11156-8", subject="Записки о путешествиях", class_room=9)
 
-# Помечаю, учебник Алгебры зарезервированым
-school_book_6.is_reserved = True
-
-print(f"Название: {school_book_6.title}, Автор: {school_book_6.author}, страниц: {school_book_6.page_count}, "
-      f"материал: {school_book_6.page_material}, "
-      f"предмет: {school_book_6.subject}, класс: {school_book_6.class_room}"
-      f"{', зарезервирована' if school_book_6.is_reserved else ''}")
+print(school_book_1.get_description())
+print(school_book_2.get_description())
+print(school_book_3.get_description())
+print(school_book_4.get_description())
+print(school_book_5.get_description())
+print(school_book_7.get_description())
