a7ae1450b50acf99e28f43646605fa190ed2b496
Grigorii Isupov <guisupov@gmail.com>
2025-01-07 18:36:43 +0300

    Homework 22 guisupov

---
+++ homework/grigory_isupov/Homework_22/load_locust.py
@@ -0,0 +1,42 @@
+from locust import task, HttpUser
+import random
+
+
+class PostsUser(HttpUser):
+    """
+    В вебинтерфейсе Locust указать этот хост: https://jsonplaceholder.typicode.com
+    """
+    headers = {'Content-type': 'application/json; charset=UTF-8'}
+    id_list = None
+    response = None
+
+    def on_start(self):
+        """Создаю список с id постов, чтобы была возможность запрашивать случайный пост"""
+        self.response = self.client.get('/posts', headers=self.headers)
+        if self.response.status_code == 200:
+            data = self.response.json()
+            self.id_list = [item['id'] for item in data]
+            print(self.id_list)
+        else:
+            print(f"Ошибка при получении постов, статус код: {self.response.status_code}")
+            self.id_list = []
+
+    @task(1)
+    def get_random_post(self):
+        if self.id_list:
+            self.client.get(
+                f'/posts/{random.choice(self.id_list)}',
+                headers=self.headers
+            )
+
+    @task(3)
+    def get_all_post(self):
+        self.client.get('/posts', headers=self.headers)
+
+    @task(2)
+    def get_comments_random_post(self):
+        if self.id_list:
+            self.client.get(
+                f'/comments?postId={random.choice(self.id_list)}',
+                headers=self.headers
+            )
